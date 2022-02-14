591794bfda43c7c0d1a30d7995642019968acb99
xiacijie <cijie.xia@mail.utoronto.ca>
2022-02-13 19:10:07 -0500

    constant pool

--- classfile/constant_info.go
+++ classfile/constant_info.go
@@ -11,7 +11,7 @@ const (
 	CONSTANT_Long = 5
 	CONSTANT_Double = 6
 	CONSTANT_NameAndType = 12
-	CONSTANT_Utf8
+	CONSTANT_Utf8 = 1
 	CONSTANT_MethodHandle = 15
 	CONSTANT_MethodType = 16
 	CONSTANT_InvokeDynamic = 18
